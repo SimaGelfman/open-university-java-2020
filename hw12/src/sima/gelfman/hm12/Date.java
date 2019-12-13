18bbc69203e373eb7c756c1457dcb05bb8e23977
Sima Gelfman <GelfmanSima@gmail.com>
2019-12-13 16:46:30 +0200

    Add setter and getter anotation, fix bug in validateDate method. Add consturctors implementation

--- hw12/src/sima/gelfman/hm12/Date.java
+++ hw12/src/sima/gelfman/hm12/Date.java
@@ -11,7 +11,7 @@ package sima.gelfman.hm12;
 
 public class Date {
     public static void main(String [] args){
-        if(validateValidDateParam(30, 12, 1000) == true)
+        if(validateDate(30, 6, 2008) == true)
             System.out.println("true");
         else System.out.println("false");
 
@@ -28,30 +28,71 @@ public class Date {
 
 
     /**
-     * Constructs a new Date with the specified date, month and minute
+     * Constructs a new Date with the specified date, month and year
      * If one parameter is incorrect - constructs a default date
      *
      * @param day  The day
-     * @param hour The hour
+     * @param month The hour
      * @param year The year
      */
 
-    public Date(int day, int hour, int year) {
-
+    public Date(int day, int month, int year) {
+        if(validateDate( day, month, year) == true){
+            _day = day;
+            _month = month;
+            _year = year;
+        }
+        else {
+            _day = DEFAULT_DAY;
+            _month = DEFAULT_MONTH;
+            _year = DEFAULT_YEAR;
+        }
 
     }
+
+    /**
+     *Constructs a new Date equivalent to the given Date
+     * @param other
+     */
 
     public Date(Date other) {
-
+        if(other != null){
+            _day = other._day;
+            _month = other._month;
+            _year = other._year;
+        }
+        else {
+            _day = DEFAULT_DAY;
+            _month = DEFAULT_MONTH;
+            _year = DEFAULT_YEAR;
+        }
     }
+
+    //getters and setters
+
+    /**
+     * Returns the day of the current object
+     *
+     * @return The _day
+     */
 
     public int getDay() {
         return _day;
     }
 
+    /**
+     * Returns the month of the current object
+     * @return The _month
+     */
+
     public int getMonth() {
         return _month;
     }
+
+    /**
+     * Returns the year of the current object
+     * @return the _year
+     */
 
     public int getYear() {
         return _year;
@@ -63,16 +104,29 @@ public class Date {
      * @param dayToSet The day to set
      */
     public void setDay(int dayToSet) {
-        if (dayToSet >= 1 && dayToSet <= 31)
+        if (validateDate( dayToSet, _month, _year) == true)
             _day = dayToSet;
     }
 
+    /**
+     * Sets the month of the object (only if the given month is a valid month)
+     *
+     * @param monthToSet The day to set
+     */
     public void setMonth(int monthToSet) {
+        if (validateDate( _day, monthToSet, _year) == true)
+            _month = monthToSet;
 
     }
 
+    /**
+     * Sets the year of the object (only if the given year is a valid year)
+     *
+     * @param yearToSet The day to set
+     */
     public void setYear(int yearToSet) {
-
+        if (validateDate( _day, _month, yearToSet) == true)
+            _year = yearToSet;
     }
 
     /**
@@ -91,12 +145,28 @@ public class Date {
         else return false;
     }
 
+
+    /**
+     *
+     * @return The String representation of the Date object
+     */
     public String toString() {
         return (_day < 10 ? "0" + _day : _day) + "/" + (_month < 10 ? "0" + _month : _month) + "/" + _year;
     }
 
+    /**
+     *
+     * @param day
+     * @param month
+     * @param year
+     * @return true when with the given day, month and year can be created valid date; @return false when with
+     * given param cannot be created valid date
+     * Param requirements are day should be between 1 and 31, month should be between 1 - 12,
+     * year should consisted 4 digits
+     * For feb
+     */
 
-    public static boolean validateValidDateParam(int day, int month, int year) {
+    private static boolean validateDate(int day, int month, int year) {
         if (year < 1000 || year > 9999)
             return false;
         if (month < 1 || month > 12)
@@ -108,16 +178,18 @@ public class Date {
                     case 7:
                     case 8:
                     case 10:
-                    case 12:
+                    case 12:{
                         if (day < 1 || day > 31)
                             return false;
-
+                        break;
+                    }
                     case 4:
                     case 6:
                     case 9:
                     case 11: {
                         if (day < 1 || day > 30)
                             return false;
+                        break;
                     }
                     case 2: {
                         if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
